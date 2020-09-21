f83e66122311ebe860758940277d2d29743e89e9
Carrie <chanscom7@gmail.com>
2020-09-21 11:19:22 -0400

    Updates

---
+++ precache-manifest.e4d36c0ce3e692f61bfd458157f7baee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e4d784d47101bcf57096e72f9fd6d8",
+    "url": "/react_projects/index.html"
+  },
+  {
+    "revision": "6f578837238b715f0440",
+    "url": "/react_projects/static/css/main.4b78c0d8.chunk.css"
+  },
+  {
+    "revision": "8130c0126c38839ba298",
+    "url": "/react_projects/static/js/2.0c79ef18.chunk.js"
+  },
+  {
+    "revision": "4edc5b53b4f6e2cf29666a6f313e7c2d",
+    "url": "/react_projects/static/js/2.0c79ef18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f578837238b715f0440",
+    "url": "/react_projects/static/js/main.e985b6a0.chunk.js"
+  },
+  {
+    "revision": "7e84545202929e8d31f4",
+    "url": "/react_projects/static/js/runtime-main.22cb1c04.js"
+  }
+]);
